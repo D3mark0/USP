71f321ca32513a426e2c7c90b20c9eaf276498eb
D3mark0 <striown@gmail.com>
2017-09-25 13:30:07 +0700

    add UTE project
    Useless Text Editor

---
+++ Useless Text Editor/Useless Text Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Useless Text Editor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Useless Text Editor")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8c9d7618-ef31-49d2-a28b-35fc7555314f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Useless Text Editor/Useless Text Editor/UTE.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Useless_Text_Editor
+{
+    public partial class UTE : Form
+    {
+        RichTextBox rtb;
+        public UTE()
+        {
+            InitializeComponent();
+
+            TabPage tp = new TabPage("Untitled");
+            RichTextBox rtb = new RichTextBox(); 
+            rtb.Dock = DockStyle.Fill;
+            tp.Controls.Add(rtb);
+            tabControl1.TabPages.Add(tp);
+        }
+
+        private RichTextBox GetRichTextBox()
+        {
+            RichTextBox rtb = null;
+            TabPage tp = tabControl1.SelectedTab;
+            if (tp != null)
+            {
+                rtb = tp.Controls[0] as RichTextBox;
+            }
+            return rtb;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TabPage tp = new TabPage("Untitled");
+            RichTextBox rtb = new RichTextBox(); 
+            rtb.Dock = DockStyle.Fill;
+            tp.Controls.Add(rtb);
+            tabControl1.TabPages.Add(tp);
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetRichTextBox().Paste();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((myStream = openFileDialog1.OpenFile()) != null)
+                {
+                    string filename = openFileDialog1.FileName;
+                    string readfiletext = File.ReadAllText(filename);
+                    TabPage tp = new TabPage("New Document");
+                    RichTextBox rtb = new RichTextBox();
+                    rtb.Dock = DockStyle.Fill;
+                    tp.Controls.Add(rtb);
+                    tabControl1.TabPages.Add(tp);
+                    rtb.Text = readfiletext;
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Filter = "*.txt(textfile)|*.txt";
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                rtb.SaveFile(savefile.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+
+            if (tabControl1.SelectedTab == null)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
